2dc52fc5116f2414b1200b3829fd0cad4f2878de
Johnata Matheus <johnata@paytour.com.br>
2025-07-14 19:23:41 -0300

    Adiciona link

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'cond-management-production-4525.up.railway.app/api',
   withCredentials: true,
 })
 
