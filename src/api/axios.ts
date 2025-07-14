1b53585e6a5d8c2042654b217e43bbdb9b9619be
Johnata Matheus <johnata@paytour.com.br>
2025-07-14 20:29:10 -0300

    Remove imports

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -5,7 +5,6 @@ const api = axios.create({
   withCredentials: true,
 })
 
-// Adiciona o token Bearer em todas as requisições se existir
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
   if (token) {
